1b4c39cae29c4b5e02ba19b4cdcfa0e534121c6b
Benjamin Bluhm <benjaminbluhm@gmx.de>
2021-01-30 20:28:38 +0100

    Deleted validation part

--- train.py
+++ train.py
@@ -4,13 +4,10 @@ from pickle import dump
 
 # Read in data
 df_train = pd.read_csv('data/titanic_train.csv', header=None)
-df_validation = pd.read_csv('data/titanic_validation.csv', header=None)
 
 # Create features and labels
 X_train = df_train.loc[:, 1:]
 y_train = df_train.loc[:, 0]
-X_val = df_validation.loc[:, 1:]
-y_val = df_validation.loc[:, 0]
 
 # Fit the model
 clf = LogisticRegression(random_state=20, max_iter=1000)

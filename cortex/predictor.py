7550e11297933ee9ebabc427836f974a1ca08ce9
Benjamin Bluhm <benjaminbluhm@gmx.de>
2021-01-30 20:27:54 +0100

    Updated cortex scripts

--- cortex/predictor.py
+++ cortex/predictor.py
@@ -9,23 +9,17 @@ class PythonPredictor:
         s3 = boto3.client("s3")
         remote_path = api.get_url(config["model_path"], repo=config["dvc_repo"])
         o = urlparse(remote_path)
-        bucket = o.netloc
-        key = o.path.lstrip('/')
+        bucket, key = o.netloc, o.path.lstrip('/')
         s3.download_file(bucket, key, "/tmp/trained_model.pkl")
         self.model = load(open("/tmp/trained_model.pkl", "rb"))
 
     def predict(self, payload):
         measurements = [
-            float(payload["1"]),
-            float(payload["2"]),
-            float(payload["3"]),
-            float(payload["4"]),
-            float(payload["5"]),
-            float(payload["6"]),
-            float(payload["7"]),
-            float(payload["8"]),
+            float(payload["1"]), float(payload["2"]), float(payload["3"]), float(payload["4"]),
+            float(payload["5"]), float(payload["6"]), float(payload["7"]), float(payload["8"]),
             float(payload["9"]),
         ]
 
-        prediction = self.model.predict([measurements])[0]
-        return str(prediction)
+        return str(self.model.predict([measurements])[0])
+
+
